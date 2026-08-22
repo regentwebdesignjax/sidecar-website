b1a783a7455bfd60beec4c25d8649ce2dbb125bb
regentwebdesignjax <regentwebdesignjax@gmail.com>
2026-08-22 11:33:49 -0400

    Update hero.tsx

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -66,7 +66,7 @@ export function Hero() {
         >
           Take control of your
           <br />
-          household budget (Together!)
+          budget – Together!
         </SplitHeadline>
 
         <p
